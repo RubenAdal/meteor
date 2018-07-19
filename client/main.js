2fab18c14cc3856947b56b44c55d73873f2c66a6
RubenAdal <rubengarcia182@gmail.com>
2018-07-19 01:10:36 -0600

    TEST

---
+++ client/main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Meteor } from 'meteor/meteor';
+
+import { render } from 'react-dom';
+
+import '../imports/startup/accounts-config.js';
+import App from '../imports/ui/App.js';
+
+import './main.html';
+ 
+
+Meteor.startup(() => {
+
+  render(<App />, document.getElementById('render-target'));
+
+});
+
+
